085599c4753bbfd3bc70732a3f112d0aab14bfeb
Anthony Guerrero <anthonyguerrero62@outlook.com>
2026-08-17 12:31:08 -0700

    added get city details

---
+++ API/citydetailsAPI.js
@@ -0,0 +1,20 @@
+export async function getCityDetails(id) {
+        // Point this to your new local Node.js server
+        const url = `http://localhost:3000/api/cities/${id}`;
+
+        try {
+                const response = await fetch(url);
+
+                if (!response.ok) {
+                        throw new Error(`HTTP error! status: ${response.status}`);
+                }
+
+                const data = await response.json();
+                return data;
+
+        } catch (error) {
+                console.error("Error fetching country data from backend:", error);
+                return [];
+        }
+}
+
