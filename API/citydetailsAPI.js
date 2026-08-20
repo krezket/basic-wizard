810f61e757781654fe324f0ff89e9138e9870707
Anthony Guerrero <anthonyguerrero62@outlook.com>
2026-08-19 19:36:16 -0700

    new styling in form

--- API/citydetailsAPI.js
+++ API/citydetailsAPI.js
@@ -1,7 +1,6 @@
 const H_URL = "https://basic-wizard-backend-04b2d718d7ba.herokuapp.com/api/citydetails/"
 
 export async function getCityDetails(id) {
-        // Point this to your new local Node.js server
         const url = `${H_URL}${id}`;
         // const url = `http://localhost:3000/api/citydetails/${searchQuery}`;
         console.log(url)
@@ -18,5 +17,5 @@ export async function getCityDetails(id) {
         } catch (error) {
                 console.error("Error fetching country data from backend:", error);
                 return [];
-        }
-}
+        };
+};
